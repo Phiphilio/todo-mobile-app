74c73dff22d698f2a06173d31576d9ca6accdc64
paoue <grappercosmo@gmail.com>
2025-01-18 23:16:34 +0100

    utilisation du composant noteInput

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
+import { NoteInput } from "@/component/noteInput";
+import { useState } from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const [text, onChangeText] = useState("");
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <NoteInput text={text} changementText={onChangeText} />
       </View>
     </SafeAreaView>
   );
@@ -16,6 +19,11 @@ const styles = StyleSheet.create({
   },
   header: {
     backgroundColor: "#A2005D",
-    flex: 1 / 7,
+    //flex: 1 / 7,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 100,
   },
 });
+
+// https://convertingcolors.com/hex-color-FF00AA.html?search=Hex(00FF00AA)
